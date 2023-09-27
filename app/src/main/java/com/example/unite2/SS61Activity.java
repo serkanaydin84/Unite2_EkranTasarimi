2476f7b4540b4a6a11fdc9c8cc8d2ea9caa3835f
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-09-27 14:59:01 +0300

    no message

---
+++ app/src/main/java/com/example/unite2/SS61Activity.java
@@ -0,0 +1,14 @@
+package com.example.unite2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SS61Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ss61);
+    }
+}
